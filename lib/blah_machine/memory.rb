2bd5ddedb522dda7b1838d63bb3d2da0f6346777
Sergei O. Udalov <sergei.udalov@gmail.com>
2012-06-24 11:21:14 +0400

    memory api changed

--- lib/blah_machine/memory.rb
+++ lib/blah_machine/memory.rb
@@ -10,26 +10,87 @@ module BlahMachine
     # Registers
     REGISTER_C0 = 0
     REGISTER_D0 = 10
+    REGISTER_D1 = 11
+
+    # status
+    REGISTER_S0 = 100
+
+    STATUS_OK = 0
+    STATUS_ADDRESS_IS_OUT_OF_RANGE = 10
+
+    REGISTER_INDEXES = [
+      REGISTER_C0,
+      REGISTER_D0,
+      REGISTER_D1,
+      REGISTER_S0
+    ]
+
+    attr_reader :data, :capacity, :registers
 
     def initialize(capacity)
       @data  = (0..capacity-1).to_a.map { MachineWord.new(nil) }
       @capacity = capacity
+
+      @registers = {}
+      initialize_registers
     end
 
-    def capacity
-      @data.size
+    def next_cycle
+      execute_current_instruction
+    end
+
+    def read_register(index)
+      if REGISTER_INDEXES.include?(index)
+        registers[index].value
+      else
+        raise UndefinedRegister.new("Register '#{index}' is udefined")
+      end
+    end
+
+    def write_register(index, value)
+      if REGISTER_INDEXES.include?(index)
+        registers[index] = MachineWord.new(value)
+      else
+        raise UndefinedRegister.new("Register '#{index}' is udefined")
+      end
+    end
+
+    private
+
+    def execute_current_instruction
+      case read_register(REGISTER_C0)
+      when IDLE
+        # do nothing
+        write_register(REGISTER_S0, STATUS_OK)
+      when READ
+        begin
+          write_register(REGISTER_D1, read(read_register(REGISTER_D0)))
+          write_register(REGISTER_S0, STATUS_OK)
+        rescue AddressIsOutOfRange
+          write_register(REGISTER_S0, STATUS_ADDRESS_IS_OUT_OF_RANGE)
+        end
+      when WRITE
+        begin
+          write(read_register(REGISTER_D0), read_register(REGISTER_D1))
+          write_register(REGISTER_S0, STATUS_OK)
+        rescue AddressIsOutOfRange
+          write_register(REGISTER_S0, STATUS_ADDRESS_IS_OUT_OF_RANGE)
+        end
+      end
+    end
+
+    def initialize_registers
+      write_register(REGISTER_C0, IDLE)
+      write_register(REGISTER_S0, STATUS_OK)
     end
 
     def write(address, value)
-      if address < 0 || address >= @capacity
-        raise AddressIsOutOfRange.new("Address '#{address}' is out of range 0 - #{@capacity}")
-      end
-
-      @data[address] = MachineWord.new(value)
+      data[address] = MachineWord.new(value)
     end
 
     def read(address)
-      @data[address].value
+      raise AddressIsOutOfRange.new("Address '#{address}' is out of range #{0} - #{capacity - 1}") if address >= capacity
+      data[address].value
     end
   end
 end
--- spec/lib/blah_machine/memory_spec.rb
+++ spec/lib/blah_machine/memory_spec.rb
@@ -3,33 +3,58 @@ require 'spec_helper.rb'
 module BlahMachine
   describe Memory do
     before(:each) do
-      @memory = Memory.new(64.kilobytes)
+      @memory = Memory.new(32.kilobytes)
     end
 
     it "should be possible to define memory size" do
-      memory = Memory.new(64.kilobytes)
-      memory.capacity.should eq(64.kilobytes)
+      memory = Memory.new(32.kilobytes)
+      memory.capacity.should eq(32.kilobytes)
     end
 
-    it "should be possible to write cell" do
-      @memory.write(0, 1)
-      @memory.read(0).should eq(1)
+    describe "WRITE" do
+      it "should be possible to write cell" do
+        @memory.write_register(Memory::REGISTER_C0, Memory::WRITE)
+        @memory.write_register(Memory::REGISTER_D0, 0)
+        @memory.write_register(Memory::REGISTER_D1, 12)
 
-      @memory.write(64.kilobytes - 1, 1)
-      @memory.read(64.kilobytes - 1).should eq(1)
+        @memory.next_cycle
+
+        @memory.data[0].value.should eq(12)
+        @memory.read_register(Memory::REGISTER_S0).should eq(Memory::STATUS_OK)
+      end
+
+      it "should be possible to write cell" do
+        @memory.write_register(Memory::REGISTER_C0, Memory::WRITE)
+        @memory.write_register(Memory::REGISTER_D0, 32.kilobytes + 1)
+        @memory.write_register(Memory::REGISTER_D1, 12)
+
+        @memory.next_cycle
+
+        @memory.read_register(Memory::REGISTER_S0).should eq(Memory::STATUS_OK)
+      end
     end
 
-    it "should not be possible use negative address" do
-      lambda do
-        @memory.write(-1, 1)
-      end.should raise_error(Memory::AddressIsOutOfRange)
-    end
+    describe "READ" do
+      it "should be possible to read cell" do
+        @memory.data[10] = MachineWord.new(76)
 
-    it "should not be possible use address out of capacity" do
-      @memory = Memory.new(64.kilobytes)
-      lambda do
-        @memory.write(64.kilobytes, 1)
-      end.should raise_error(Memory::AddressIsOutOfRange)
+        @memory.write_register(Memory::REGISTER_C0, Memory::READ)
+        @memory.write_register(Memory::REGISTER_D0, 10)
+
+        @memory.next_cycle
+
+        @memory.read_register(Memory::REGISTER_D1).should eq(76)
+        @memory.read_register(Memory::REGISTER_S0).should eq(Memory::STATUS_OK)
+      end
+
+      it "should return error status if address is out of range" do
+        @memory.write_register(Memory::REGISTER_C0, Memory::READ)
+        @memory.write_register(Memory::REGISTER_D0, 32.kilobytes + 1)
+
+        @memory.next_cycle
+
+        @memory.read_register(Memory::REGISTER_S0).should eq(Memory::STATUS_ADDRESS_IS_OUT_OF_RANGE)
+      end
     end
   end
 end
