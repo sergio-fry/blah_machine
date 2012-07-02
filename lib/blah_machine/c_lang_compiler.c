7dcb0270c10a60680c554baa4dee0e661916ee75
Sergei O. Udalov <sergei.udalov@gmail.com>
2012-07-02 10:17:55 +0400

    statements detection

--- lib/blah_machine/c_lang_compiler.c
+++ lib/blah_machine/c_lang_compiler.c
@@ -57,7 +57,14 @@ const int STATE_FD_ARGUMENT_TYPE = 15;
 const int STATE_FD_ARGUMENT_NAME = 16;
 const int STATE_FD_ARGUMENTS_LIST_END = 17;
 const int STATE_FD_BODY_START = 18;
-const int STATE_FD_BODY_END = 19;
+const int STATE_FD_BODY = 19;
+const int STATE_FD_BODY_END = 20;
+
+// Variable defenition
+const int STATE_VD_TYPE = 30;
+const int STATE_VD_NAME = 31;
+
+const STATE_STATEMENT = 40;
 
 int is_identifier_first_char(char ch) {
   return (ch >= 'a' && ch <= 'z') || 
@@ -70,6 +77,23 @@ int is_identifier_char(char ch) {
     (ch >= 'A' && ch <= 'Z') ||
     (ch >= '0' && ch <= '9') ||
     ch == '_';
+}
+
+int is_identifier(char* str) {
+  int length = strlen(str), result = 1;
+
+  int i = 0;
+  while(i < length) {
+    if((i == 0 && !is_identifier_first_char(str[i])) ||
+        !is_identifier_char(str[i])) {
+      result = 0;
+      break;
+    }
+
+    i++;
+  }
+
+  return result;
 }
 
 int is_space(char ch) {
@@ -104,9 +128,27 @@ int next_state (int* current_state, char* token) {
       *current_state = STATE_FD_BODY_START;
     }
   } else if(*current_state == STATE_FD_BODY_START) {
+    if(strcmp(token, "}") != 0) {
+      *current_state = STATE_FD_BODY;
+      next_state(current_state, token);
+    }
+  } else if(*current_state == STATE_FD_BODY) {
     if(strcmp(token, "}") == 0) {
       *current_state = STATE_FD_BODY_END;
+    } else if(strcmp(token, "int") == 0) {
+      *current_state = STATE_VD_TYPE;
+    } else if(is_identifier(token)) {
+      *current_state = STATE_STATEMENT;
     }
+  } else if (*current_state == STATE_STATEMENT) {
+    if(strcmp(token, ";") == 0) {
+      *current_state = STATE_FD_BODY;
+    }
+  } else if(*current_state == STATE_VD_TYPE) {
+    *current_state = STATE_VD_NAME;
+
+  } else if(*current_state == STATE_VD_NAME) {
+    *current_state = STATE_FD_BODY;
   } else if(*current_state == STATE_FD_BODY_END) {
     *current_state = STATE_FD_TYPE;
   }
@@ -141,11 +183,14 @@ int read_next_token(char* source_code, int* current_position, char* token) {
 }
 
 const int FUNCTIONS_TABLE_MAX_SIZE = 255;
+const int VARIABLES_TABLE_MAX_SIZE = 255;
 
 int main() {
   char* source_code = "\
   int sum(int a, int b) {\
-    return a + b; \
+    int c; \
+    c = a + b; \
+    return c; \
   } \
   int main() { \n\
     return 0; \n\
@@ -165,12 +210,7 @@ int main() {
 
     next_state(&current_state, token);
 
-
-    if(current_state == STATE_FD_NAME) {
-      strcpy(token, functions_table[functions_count++]);
-    }
-
-    /*printf("%s %d\n", token, current_state);*/
+    printf("%2d | %s \n", current_state, token);
   }
 
   return 0;
